e137b123357d648963f6c2e331182ea0768d1674
reeseo3o <notyaeji@gmail.com>
2024-01-06 12:59:54 +0000

    Feat: 공통 컴포넌트 BottomNavBar 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from '@/components/ui/Toaster';
+import BottomNavBar from '../components/ui/BottomNavBar';
+import Providers from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,9 +19,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} w-full bg-slate-100`}>
-        <div className="m-auto h-screen max-w-[600px] bg-white">{children}</div>
-        <Toaster />
+      <body className={`${inter.className} relative w-full bg-slate-100`}>
+        <Providers>
+          <div className="m-auto h-screen max-w-[600px] bg-white">
+            {children}
+          </div>
+          <Toaster />
+          <div className="fixed bottom-0">{/* <BottomNavBar /> */}</div>
+        </Providers>
       </body>
     </html>
   );
