63467171067f0b9eba2249dd343468e4e796f0ae
reeseo3o <notyaeji@gmail.com>
2024-09-03 13:51:47 +0000

    Fix: build error 수정 - 빌드 시점에 token 다루는 컴포넌트/페이지 dynamic import 처리

--- src/components/signin/ResetPwdContent.tsx
+++ src/components/signin/ResetPwdContent.tsx
@@ -1,90 +1,58 @@
-'use client';
-
-import { useState } from 'react';
 import { Input } from '../../components/ui/Input';
 import { Button } from '../../components/ui/Button';
-import { useUser } from '@/contexts/EmailContext';
+import { resetPwdOnServer } from '../../services/signup';
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
-import { signUpOnServer } from '@/services/signup';
-import { useRouter } from 'next/navigation';
-import SignupAgreeBottomSheet from '../../components/signup/SignupAgreeBottomSheet';
-import usePasswordHandler from '@/utils/usePasswordHandler';
+import usePasswordHandler from '../../utils/usePasswordHandler';
 import Modal from '@/components/ui/Modal';
-import { signInOnServer } from '@/services/signin';
 
-const SignUpPage = () => {
+const ResetPwdContent = () => {
+  const searchParams = useSearchParams();
   const router = useRouter();
 
-  const [openAgreeBottom, setOpenAgreeBottom] = useState(false);
+  const [token, setToken] = useState('');
   const [openModal, setOpenModal] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
 
   const { password, passwordCheck, handlePwdChange, handlePwdCheckChange } =
     usePasswordHandler();
 
-  const { email } = useUser();
+  useEffect(() => {
+    const token = searchParams.get('token');
+    if (!token) return;
 
-  const openTermsOfUseModal = () => {
-    console.log('openTermsOfUse');
-  };
-  const openPrivacyModal = () => {
-    console.log('openPrivacy');
-  };
+    setToken(token);
+  }, []);
 
-  const signUp = () => {
-    signUpMutation.mutate({
-      email: email,
-      password: password.value,
-      loginType: 'EMAIL'
-    });
-  };
-
+  /**
+   * @description API - POST reset password
+   */
   const signUpMutation = useMutation({
-    mutationFn: (reqObj: {
-      email: string;
-      password: string;
-      loginType: string;
-    }) => signUpOnServer(reqObj),
+    mutationFn: (reqObj: { password: string; token: string }) =>
+      resetPwdOnServer(reqObj),
     onSuccess: (data: any) => {
       if (data.status !== 'OK') {
         data.message && setErrorMsg(data.message);
         setOpenModal(true);
       } else {
-        signInOnServer({
-          email: email,
-          password: password.value
-        });
-        signInMutation.mutate({ email: email, password: password.value });
+        router.push('/signin');
       }
     }
   });
 
-  const signInMutation = useMutation({
-    mutationFn: (reqObj: { email: string; password: string }) => {
-      return signInOnServer(reqObj);
-    },
-    onSuccess: (response: any) => {
-      if (response.status === 200) {
-        router.push('/onboard');
-      } else {
-        console.error('로그인 중 오류:', response.message);
-        router.push('/signin');
-      }
-    },
-    onError: (error: any) => {
-      console.error('로그인 중 오류:', error);
-    }
-  });
+  const resetPwd = () => {
+    signUpMutation.mutate({ password: password.value, token: token });
+  };
 
   return (
     <section className="px-4 pt-10">
       <article className="pb-8 text-left text-heading-1 text-gr-black">
-        <h1>비밀번호 설정하고</h1>
-        <h1>회원가입을 완료해 주세요</h1>
+        <h1>새 비밀번호를 입력하여</h1>
+        <h1>재설정하세요</h1>
       </article>
       <article className="flex flex-col gap-2">
         <Input
-          type="password"
           helperText={
             password.error ? '8자 이상 / 영문, 숫자, 특수문자 가능' : ''
           }
@@ -94,7 +62,6 @@ const SignUpPage = () => {
           onChange={handlePwdChange}
         />
         <Input
-          type="password"
           helperText={passwordCheck.error ? '비밀번호를 확인해주세요' : ''}
           value={passwordCheck.value}
           placeholder="비밀번호 확인"
@@ -115,16 +82,11 @@ const SignUpPage = () => {
               ? true
               : false
           }
-          onClick={() => setOpenAgreeBottom(true)}
+          onClick={resetPwd}
         >
-          가입하기
+          재설정하기
         </Button>
       </article>
-      <SignupAgreeBottomSheet
-        open={openAgreeBottom}
-        setIsVisible={setOpenAgreeBottom}
-        onClick={signUp}
-      />
       {openModal && (
         <Modal
           contents={{ title: '알림', body: errorMsg }}
@@ -135,9 +97,7 @@ const SignUpPage = () => {
               size: 'lg',
               content: '확인',
               style: 'w-full rounded-[16px] px-4 py-2 bg-sm-error-700',
-              onClick: () => {
-                setOpenModal(false), setOpenAgreeBottom(false);
-              }
+              onClick: () => setOpenModal(false)
             }
           ]}
         />
@@ -146,5 +106,4 @@ const SignUpPage = () => {
   );
 };
 
-SignUpPage.displayName = 'SignUpPage';
-export default SignUpPage;
+export default ResetPwdContent;
