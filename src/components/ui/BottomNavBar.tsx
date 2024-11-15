26f259099f2ce262a5c22731451c4661ab22ef2c
reeseo3o <notyaeji@gmail.com>
2024-11-15 06:54:01 +0000

    Fix: BottomNavBar 프로필 API 연결로 프로필이미지 적용

--- src/components/ui/BottomNavBar.tsx
+++ src/components/ui/BottomNavBar.tsx
@@ -5,6 +5,8 @@ import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import Badge from '@/components/ui/Badge';
+import { useQuery } from '@tanstack/react-query';
+import { getMyProfile } from '@/services/profile';
 
 const BOTTOM_NAV = [
   {
@@ -35,42 +37,60 @@ const BOTTOM_NAV = [
     key: 'profile',
     value: '프로필',
     img: {
-      active: 'https://github.com/shadcn.png',
-      default: 'https://github.com/shadcn.png'
+      active:
+        'https://meowzip.s3.ap-northeast-2.amazonaws.com/images/icon/profile/can.svg',
+      default:
+        'https://meowzip.s3.ap-northeast-2.amazonaws.com/images/icon/profile/can.svg'
     }
   }
 ];
+
 const BottomNavBar = () => {
   const [activeNav, setActiveNav] = useState('diary');
-
   const pathName = usePathname();
+
+  const { data: myProfile, isLoading } = useQuery({
+    queryKey: ['myProfile'],
+    queryFn: () => getMyProfile()
+  });
 
   useEffect(() => {
     const path = pathName.split('/')[1];
     setActiveNav(path);
   }, [pathName]);
 
+  if (isLoading) return;
+
   return (
     <div className="flex w-screen max-w-[600px] justify-center rounded-t-[20px] bg-gr-white px-2 pb-[34px] pt-2 shadow-bottomNav">
-      {BOTTOM_NAV.map(nav => (
-        <Link key={nav.key} href={`/${nav.key}`} className="relative px-4">
-          {nav.key === 'profile' && (
-            <div className="absolute right-4">
-              <Badge type="default" bgColor="bg-pr-500" />
-            </div>
-          )}
-          <Image
-            src={nav.key === activeNav ? nav.img.active : nav.img.default}
-            alt={nav.key}
-            width={40}
-            height={40}
-            className="rounded-full p-[3px]"
-          />
-          <h5 className="text-center text-[10px] font-normal text-gr-800">
-            {nav.value}
-          </h5>
-        </Link>
-      ))}
+      {BOTTOM_NAV.map(nav => {
+        const imgSrc =
+          nav.key === 'profile'
+            ? myProfile.profileImageUrl || nav.img.default
+            : nav.key === activeNav
+              ? nav.img.active
+              : nav.img.default;
+
+        return (
+          <Link key={nav.key} href={`/${nav.key}`} className="relative px-4">
+            {nav.key === 'profile' && (
+              <div className="absolute right-4">
+                <Badge type="default" bgColor="bg-pr-500" />
+              </div>
+            )}
+            <Image
+              src={imgSrc}
+              alt={nav.key}
+              width={40}
+              height={40}
+              className="rounded-full p-[3px]"
+            />
+            <h5 className="text-center text-[10px] font-normal text-gr-800">
+              {nav.value}
+            </h5>
+          </Link>
+        );
+      })}
     </div>
   );
 };
